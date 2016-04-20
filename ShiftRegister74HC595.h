be79c4275d93c7fd572acfe8a4fab9871d5a71e2
Timo Denk <simssosdenk@googlemail.com>
2016-04-20 16:08:52 +0200

    Correct indenting

--- ShiftRegister74HC595.h
+++ ShiftRegister74HC595.h
@@ -12,21 +12,21 @@
 
 class ShiftRegister74HC595 
 {
-    public:
-        ShiftRegister74HC595(int numberOfShiftRegisters, int serialDataPin, int clockPin, int latchPin);
-        void setAll(uint8_t * digitalValues);
-        uint8_t * getAll(); 
-        void set(int pin, uint8_t value);
-        void setAllLow();
-        void setAllHigh(); 
-        uint8_t get(int pin);
+public:
+    ShiftRegister74HC595(int numberOfShiftRegisters, int serialDataPin, int clockPin, int latchPin);
+    void setAll(uint8_t * digitalValues);
+    uint8_t * getAll(); 
+    void set(int pin, uint8_t value);
+    void setAllLow();
+    void setAllHigh(); 
+    uint8_t get(int pin);
 
-    private:
-        int _numberOfShiftRegisters;
-        int _clockPin;
-        int _serialDataPin;
-        int _latchPin;
-        uint8_t * _digitalValues;
+private:
+    int _numberOfShiftRegisters;
+    int _clockPin;
+    int _serialDataPin;
+    int _latchPin;
+    uint8_t * _digitalValues;
 };
 
 #endif
