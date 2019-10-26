ec75fa3cf58b25a0e35f300669130d9b101824bd
lathoub <bart.de.lathouwer@telenet.be>
2019-10-26 10:06:12 +0200

    syntactical rewrite using c++ templates to avoid heap fragmentation
    - avoid using free/malloc as it causes heap fragmentation and crashes long term (https://arduino.stackexchange.com/questions/682/is-using-malloc-and-free-a-really-bad-idea-on-arduino/712) when repeatedly calling constructor/destructor (this modif avoids that)
    - use uint8_t in stead of int (saving memory)

--- examples/example/example.ino
+++ examples/example/example.ino
@@ -8,8 +8,8 @@
 #include <ShiftRegister74HC595.h>
 
 // create a global shift register object
-// parameters: (number of shift registers, data pin, clock pin, latch pin)
-ShiftRegister74HC595 sr (1, 0, 1, 2); 
+// parameters: <number of shift registers> (data pin, clock pin, latch pin)
+ShiftRegister74HC595<1> sr(0, 1, 2);
  
 void setup() { 
 }
